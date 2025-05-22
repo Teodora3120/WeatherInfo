2e9fffd8e80b3e8f5cae3f47a9598b983ea41490
Stefan <71078992+Teodora3120@users.noreply.github.com>
2025-05-22 16:13:55 +0300

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,13 @@
 import pytest
+from httpx import AsyncClient, ASGITransport
 from app.main import app
-from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_get_weather():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/weather", params={"city" : "Iasi"})
+    transport = ASGITransport(app=app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        response = await ac.get("/weather", params={"city": "Iasi"})
+    
     assert response.status_code == 200
 
     json_data = response.json()
